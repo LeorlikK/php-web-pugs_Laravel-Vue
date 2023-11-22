569ce22a4fac0fafa07476d335c7880a9b45355c
LeorlikK <leorl1k@yandex.ru>
2023-11-23 00:07:48 +0300

    socket

---
+++ app/Events/LoadingVideoEvent.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class LoadingVideoEvent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(readonly int $id, readonly string $percent)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('loading_video_' . $this->id),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'loading_video_push';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'percent' => $this->percent,
+        ];
+    }
+}
