c36e84bdf9d09508d34ffd348451d3ff3e8a627b
LeorlikK <leorl1k@yandex.ru>
2023-11-30 08:05:53 +0300

    comments-chat

---
+++ app/Events/NewCommentEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\CommentsResource;
+use App\Models\Comment;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NewCommentEvent  implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(readonly Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('news_comments'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'news_comments';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'comment' => CommentsResource::make($this->comment),
+        ];
+    }
+}
